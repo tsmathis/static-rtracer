7f2cfca5f50d3645e03b211a38682b62c95b586e
Tyler S. Mathis <35553152+tsmathis@users.noreply.github.com>
2023-01-12 22:36:34 +0100

    add sample renders

--- src/main.cpp
+++ src/main.cpp
@@ -1,266 +1,8 @@
 #include "common_consts.h"
-
-#include "axis_aligned_rect.h"
-#include "box.h"
-#include "bvh.h"
-#include "camera.h"
-#include "color.h"
-#include "constatn_medium.h"
-#include "hittable_list.h"
-#include "material.h"
-#include "moving_sphere.h"
-#include "sphere.h"
-#include "texture.h"
+#include "scenes.h"
 
 #include <iostream>
 #include <time.h>
-
-color ray_color(const ray& r, const color& background, const hittable& world, int depth) {
-	hit_record rec;
-
-	if (depth <= 0)
-		return color(0, 0, 0);
-
-	if (!world.hit(r, 0.001, infinity, rec))
-		return background;
-
-	ray scattered;
-	color attenuation;
-	color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
-
-	if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-		return emitted;
-
-	return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
-}
-
-hittable_list simple_light() {
-	hittable_list objects;
-
-	auto perlin_text = make_shared<noise_texture>(4);
-	objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(perlin_text)));
-	objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(perlin_text)));
-
-	auto diff_light = make_shared<diffuse_light>(color(4, 4, 4));
-	auto dim_light = make_shared<diffuse_light>(color(2, 2, 2));
-
-	objects.add(make_shared<xy_rect>(3, 5, 1, 3, -2, diff_light));
-	objects.add(make_shared<sphere>(point3(5, 6, 4), 1, dim_light));
-
-	return objects;
-}
-
-
-
-hittable_list random_scene() {
-	hittable_list world;
-
-	auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
-
-	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
-
-	//auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-	//world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
-
-	for (int a = -7; a < 7; a++) {
-		for (int b = -7; b < 7; b++) {
-			auto choose_mat = random_double();
-			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
-
-			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-				shared_ptr<material> sphere_material;
-
-				if (choose_mat < 0.8) {
-					// diffuse
-					auto albedo = color::random() * color::random();
-					sphere_material = make_shared<lambertian>(albedo);
-					auto center2 = center + vec3(0, random_double(0, 0.5), 0);
-					world.add(make_shared<moving_sphere>(
-						center, center2, 0.0, 1.0, 0.2, sphere_material
-						)
-					);
-				}
-				else if (choose_mat < 0.95) {
-					// metal
-					auto albedo = color::random(0.5, 1);
-					auto fuzz = random_double(0, 0.5);
-					sphere_material = make_shared<metal>(albedo, fuzz);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
-				else {
-					// glass
-					sphere_material = make_shared<dielectric>(1.5);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
-			}
-		}
-	}
-
-	auto material1 = make_shared<dielectric>(1.5);
-	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-	return world;
-
-}
-
-hittable_list two_spheres() {
-	hittable_list objects;
-
-	auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
-
-	objects.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
-	objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
-
-	return objects;
-}
-
-hittable_list two_perlin_spheres() {
-	hittable_list objects;
-
-	auto perlin_texture = make_shared<noise_texture>(4);
-
-	objects.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(perlin_texture)));
-	objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(perlin_texture)));
-
-	return objects;
-}
-
-hittable_list earth() {
-	auto earth_texture = make_shared<image_texture>("earthmap.jpg");
-	auto earth_surface = make_shared<lambertian>(earth_texture);
-	auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
-
-	return hittable_list(globe);
-}
-
-hittable_list cornell_box() {
-	hittable_list objects;
-
-	auto red   = make_shared<lambertian>(color(0.65, 0.05, 0.05));
-	auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
-	auto green = make_shared<lambertian>(color(0.12, 0.45, 0.15));
-	auto light = make_shared<diffuse_light>(color(15, 15, 15));
-
-	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
-	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
-	objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
-	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
-	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
-	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
-
-	shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
-	box1 = make_shared<rotate_y>(box1, 15);
-	box1 = make_shared<translate>(box1, vec3(265, 0, 295));
-	objects.add(box1);
-
-	shared_ptr<hittable> box2 = make_shared<box>(point3(0 ,0, 0), point3(165, 165, 165), white);
-	box2 = make_shared<rotate_y>(box2, -18);
-	box2 = make_shared<translate>(box2, vec3(130, 0, 65));
-	objects.add(box2);
-
-	return objects;
-}
-
-hittable_list cornell_smoke() {
-	hittable_list objects;
-
-	auto red = make_shared<lambertian>(color(0.65, 0.05, 0.05));
-	auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
-	auto green = make_shared<lambertian>(color(0.12, 0.45, 0.15));
-	auto light = make_shared<diffuse_light>(color(7, 7, 7));
-
-	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
-	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
-	objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
-	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
-	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
-	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
-
-	shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
-	box1 = make_shared<rotate_y>(box1, 15);
-	box1 = make_shared<translate>(box1, vec3(265, 0, 295));
-
-	shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
-	box2 = make_shared<rotate_y>(box2, -18);
-	box2 = make_shared<translate>(box2, vec3(130, 0, 65));
-
-	objects.add(make_shared<constant_medium>(box1, 0.01, color(0, 0, 0)));
-	objects.add(make_shared<constant_medium>(box2, 0.01, color(1, 1, 1)));
-
-	return objects;
-}
-
-hittable_list feature_ensemble() {
-	hittable_list floor_boxes;
-	auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
-
-	const int boxes_per_side = 20;
-	for (int i = 0; i < boxes_per_side; i++) {
-		for (int j = 0; j < boxes_per_side; j++) {
-			auto w  =  100.0;
-			auto x0 = -1000.0 + i * w;
-			auto z0 = -1000.0 + j * w;
-			auto y0 =  0.0;
-			auto x1 =  x0 + w;
-			auto y1 =  random_double(1, 101);
-			auto z1 =  z0 + w;
-
-			floor_boxes.add(make_shared<box>(point3(x0, y0, z0), point3(x1, y1, z1), ground));
-		}
-	}
-
-	hittable_list objects;
-
-	objects.add(make_shared<bvh_node>(floor_boxes, 0, 1));
-
-	auto light = make_shared<diffuse_light>(color(7, 7, 7));
-	objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
-
-	auto center1 = point3(400, 400, 200);
-	auto center2 = center1 + vec3(30, 0, 0);
-	auto moving_sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
-	objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
-
-	objects.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
-	objects.add(make_shared<sphere>(
-		point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)
-		));
-
-	auto boundary = make_shared<sphere>(point3(360, 150, 145), 70, make_shared<dielectric>(1.5));
-	objects.add(boundary);
-	objects.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
-	boundary = make_shared<sphere>(point3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
-	objects.add(make_shared<constant_medium>(boundary, 0.0001, color(1, 1, 1)));
-
-	auto earth = make_shared<lambertian>(make_shared<image_texture>("earthmap.jpg"));
-	objects.add(make_shared<sphere>(point3(400, 200, 400), 100, earth));
-
-	auto perlin_texture = make_shared<noise_texture>(0.1);
-	objects.add(make_shared<sphere>(point3(220, 280, 300), 80, make_shared<lambertian>(perlin_texture)));
-
-	hittable_list sphere_cube;
-	auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
-	int n = 1000;
-	for (int j = 0; j < n; j++) {
-		sphere_cube.add(make_shared<sphere>(point3::random(0, 165), 10, white));
-	}
-
-	objects.add(make_shared<translate>(
-		make_shared<rotate_y>(
-			make_shared<bvh_node>(sphere_cube, 0.0, 1.0), 15),
-		    vec3(-100, 270, 395)
-		)
-	);
-
-	return objects;
-}
-
 
 int main() {
 	time_t begin, end;
@@ -270,7 +12,7 @@ int main() {
 	auto aspect_ratio = 16.0 / 9.0;
 	int image_width = 400;
 	int samples_per_pixel = 80;
-	int max_depth = 30;
+	int max_depth = 8;
 
 	// World
 	hittable_list world;
@@ -332,7 +74,7 @@ int main() {
 		world = cornell_box();
 		aspect_ratio = 1.0;
 		image_width = 600;
-		samples_per_pixel = 200;
+		samples_per_pixel = 800;
 		background = color(0, 0, 0);
 		lookfrom = point3(278, 278, -1800);
 		lookat = point3(278, 278, 0);
@@ -344,7 +86,7 @@ int main() {
 		world = cornell_smoke();
 		aspect_ratio = 1.0;
 		image_width = 600;
-		samples_per_pixel = 200;
+		samples_per_pixel = 800;
 		background = color(0, 0, 0);
 		lookfrom = point3(278, 278, -1800);
 		lookat = point3(278, 278, 0);
@@ -356,7 +98,7 @@ int main() {
 		world = feature_ensemble();
 		aspect_ratio = 1.0;
 		image_width = 800;
-		samples_per_pixel = 100;
+		samples_per_pixel = 1800;
 		background = color(0, 0, 0);
 		lookfrom = point3(478, 278, -1600);
 		lookat = point3(278, 278, 0);
