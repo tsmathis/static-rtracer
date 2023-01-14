630c1061fcbc7b812189849963e04f3db9786a22
tmathis <tmathis204@gmail.com>
2023-01-14 11:48:31 +0100

    begin refactor

--- src/scenes.h
+++ src/scenes.h
@@ -7,7 +7,7 @@
 #include "bvh.h"
 #include "camera.h"
 #include "color.h"
-#include "constatn_medium.h"
+#include "constant_medium.h"
 #include "hittable_list.h"
 #include "material.h"
 #include "moving_sphere.h"
